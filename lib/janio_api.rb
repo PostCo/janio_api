b8bd4cf1c9a0d40be4f3ff1a573f6bbf500d3968
Andy Chong <andy_gg1996@hotmail.com>
2020-04-14 21:26:23 +0800

    configure zeitwerk

--- lib/janio_api.rb
+++ lib/janio_api.rb
@@ -1,7 +1,24 @@
 require "janio_api/version"
 require "active_resource"
 require "dotenv/load"
-require_relative "zeitwerk_loader"
+# require_relative "zeitwerk_loader"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect(
+  "janio_api" => "JanioAPI"
+)
+loader.collapse("./lib/janio_api/resources")
+loader.enable_reloading
+loader.tag = "janio_api_gem"
+# loader.log!
+loader.setup
+$__janio_api_loader__ = loader
+if ENV["JANIO_API_GEM_ENV"] == "development"
+  def reload!
+    $__janio_api_loader__.reload
+  end
+end
 
 module JanioAPI
   class << self
